37d20af575689460ecd2d4cc72a41e23727a964a
devitoolz <tkdfuf0923@gmail.com>
2023-06-26 11:49:54 +0900

    Feat: Main UI 구성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
-
+import { Layout } from 'antd';
+import { Header, Content } from './styles/AppStyle';
 const App = () => {
-  return <div>App</div>;
-};
+  // const [collapsed, setCollapsed] = useState(false);
 
+  return (
+    <Layout>
+      <Header>
+        <span>Travel Todo</span>
+      </Header>
+      <Content>
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            background: 'white',
+            borderRadius: 10,
+          }}
+        >
+          Map
+        </div>
+      </Content>
+    </Layout>
+  );
+};
 export default App;
---
+++ src/styles/AppStyle.js
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled';
+import { Layout } from 'antd';
+
+const Header = styled(Layout.Header)`
+  position: fixed;
+  width: 100%;
+  height: 70px;
+  z-index: 9999;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  background: #1e88e5;
+  box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.5);
+  > span {
+    font-size: 30px;
+    font-weight: bold;
+    color: #fff;
+    margin-left: 20px;
+  }
+`;
+
+const Content = styled(Layout.Content)`
+  height: calc(100vh - 70px);
+  margin-top: 70px;
+  display: flex;
+  padding: 15px;
+`;
+
+export { Header, Content };
