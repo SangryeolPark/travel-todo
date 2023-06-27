4df1d4de4941bb1b19ae7e17e633332f05e2e128
heeheepark <pjh9641@gmail.com>
2023-06-27 21:08:57 +0900

    feat: calendar 페이지 언어 변경

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import './../styles/calendar.css';
@@ -23,17 +23,41 @@ const Calendar = ({ originData }) => {
     };
     travelList = [...travelList, eventData];
   });
+  // const test = moment('June 2023').format(`'YYYY년 M월'`);
+
+  useEffect(() => {
+    const headerCell = document.querySelectorAll('.fc-col-header-cell-cushion');
+    const day = ['일', '월', '화', '수', '목', '금', '토'];
+    headerCell.forEach((item, index) => (item.innerHTML = day[index]));
+
+    // let test = document.querySelector('.fc-toolbar-title');
+    // const newMonth = moment(test.innerHTML).format(`YYYY년 M월`);
+    // console.log(newMonth);
+
+    // test.innerHTML = newMonth;
+    // console.log(test.innerHTML);
+
+    // const dayNum = document.querySelectorAll('.fc-daygrid-day-number');
+    // dayNum.forEach(item => (item.innerHTML = item.innerHTML.replace('일', '')));
+  }, []);
 
   return (
     <div className="wrap">
       <FullCalendar
         height="calc(100vh - 130px)"
         initialView="dayGridMonth"
+        // locale="ko"
+        // locales={['esLocale', 'koLocale']}
+        titleFormat={{
+          month: '2-digit',
+          year: 'numeric',
+          locale: 'ko',
+        }}
+        // dayCellContent={{ locale: 'ko' }}
         plugins={[dayGridPlugin]}
         events={travelList}
       />
     </div>
   );
 };
-
 export default Calendar;
