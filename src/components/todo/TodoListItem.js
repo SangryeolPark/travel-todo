b9424f844ded2d09920cb3e0080e0444fd10b1e8
heeheepark <pjh9641@gmail.com>
2023-06-28 12:02:12 +0900

    feat: 일정 삭제 기능 추가

--- src/components/todo/TodoListItem.js
+++ src/components/todo/TodoListItem.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import TodoCheckList from './TodoCheckList';
 
 const TodoListItem = ({ item, data, setData }) => {
+  console.log(item);
   const checkList = item.checkList;
 
+  // checkList 추가
   const addCheckList = _id => {
     const newCheckList = {
       id: Date.now(),
@@ -11,10 +13,10 @@ const TodoListItem = ({ item, data, setData }) => {
       complete: false,
     };
     const checkList = [...item.checkList, newCheckList];
-    const newVisitList = { ...item, checkList: checkList };
+    const visitList = { ...item, checkList: checkList };
     const newVisitListData = data.visitList.map(item => {
-      if (item.id === newVisitList.id) {
-        item = newVisitList;
+      if (item.id === visitList.id) {
+        item = visitList;
       }
       return item;
     });
@@ -22,21 +24,30 @@ const TodoListItem = ({ item, data, setData }) => {
     setData(newData);
   };
 
-  const [inputValue, setInputValue] = useState(item.title);
-  const handleInput = e => {
-    setInputValue(e.target.value);
+  // Todo 삭제
+  const deleteTodo = _id => {
+    const newVisitListData = data.visitList.filter(item => item.id !== _id);
+    console.log(newVisitListData);
+    const newData = { ...data, visitList: newVisitListData };
+    console.log(newData);
+    setData(newData);
   };
+
+  // const [inputValue, setInputValue] = useState(item.title);
+  // const handleInput = e => {
+  //   setInputValue(e.target.value);
+  // };
   return (
     <div>
       <input type="checkbox" defaultChecked={item.complete} />
       <input
         type="text"
-        value={inputValue}
-        onChange={handleInput}
+        value={item.title}
+        // onChange={handleInput}
         style={{ border: 'none', background: 'transparent', fontSize: 16, fontWeight: 600 }}
       />
       <button onClick={() => addCheckList(item.id)}>준비추가</button>
-      <button>일정삭제</button>
+      <button onClick={() => deleteTodo(item.id)}>일정삭제</button>
       <div style={{ marginLeft: 15 }}>
         {checkList &&
           checkList.map((item, index) => <TodoCheckList key={index} checkList={item} />)}
