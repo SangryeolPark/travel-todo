1909638d4697e26e6dde6d38f41a14844ffe7053
heeheepark <pjh9641@gmail.com>
2023-07-06 17:28:04 +0900

    feat: calendar 상세 일정 페이지 모달창 추가

--- src/components/calendar/Schedule.js
+++ src/components/calendar/Schedule.js
@@ -1,19 +1,35 @@
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashCan, faPencil, faSquareCheck } from '@fortawesome/free-solid-svg-icons';
 import { faSquare } from '@fortawesome/free-regular-svg-icons';
-import { Input } from 'antd';
+import { Input, Modal } from 'antd';
 import TextArea from 'antd/es/input/TextArea';
 import { DetailScheduleDiv } from '../../styles/CalendarStyle';
+import { useState } from 'react';
 
 const Schedule = ({ selectTitle, selectStartDate, selectEndDate, selectReview, todoData }) => {
+  const navigate = useNavigate();
+  // 일정 삭제 모달창
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleEdit = () => {};
+
   return (
     <DetailScheduleDiv>
       <div className="btns">
         <Link to="/todo">
-          <FontAwesomeIcon icon={faPencil} className="bt-pencil" />
+          <FontAwesomeIcon icon={faPencil} className="bt-pencil" onClick={handleEdit} />
         </Link>
-        <FontAwesomeIcon icon={faTrashCan} className="bt-trash" /*onClick={onClose}*/ />
+        <FontAwesomeIcon icon={faTrashCan} className="bt-trash" onClick={showModal} />
       </div>
       <div className="travel-schedule">
         <h2>Travel Schedule</h2>
@@ -24,7 +40,7 @@ const Schedule = ({ selectTitle, selectStartDate, selectEndDate, selectReview, t
         <h2>Travel Plan</h2>
         <div className="detail-plan">
           {todoData.map(item => (
-            <div key={item.idSub}>
+            <div key={item.idSub} className="visit-list-wrap">
               <div className="visit-list">
                 <FontAwesomeIcon
                   icon={item.finishYn ? faSquareCheck : faSquare}
@@ -54,6 +70,25 @@ const Schedule = ({ selectTitle, selectStartDate, selectEndDate, selectReview, t
       <div className="travel-review">
         <h2>Travel Review</h2>
         <TextArea className="text-area" value={selectReview} disabled={true} rows={5} />
+      </div>
+      <div>
+        <Modal
+          title="확인"
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          className="modal"
+          // style={{
+          //   position: 'absolute',
+          //   top: '50%',
+          //   width: '520px',
+          //   transform: 'translate(-50%, -50%)',
+          //   left: '50%',
+          // }}
+        >
+          <p>삭제한 일정은 복구할 수 없습니다.</p>
+          <p>정말 삭제하시겠습니까?</p>
+        </Modal>
       </div>
     </DetailScheduleDiv>
   );
--- src/styles/CalendarStyle.js
+++ src/styles/CalendarStyle.js
@@ -60,6 +60,13 @@ export const DrawerDiv = styled.div`
 export const DetailScheduleDiv = styled.div`
   padding-top: 60px;
   position: relative;
+  .modal {
+    position: absolute;
+    top: 50%;
+    width: 520px;
+    transform: translate(-50%, -50%);
+    left: 50%;
+  }
   .btns {
     color: #494949;
     position: absolute;
@@ -112,42 +119,19 @@ export const DetailScheduleDiv = styled.div`
       border: 1px solid rgb(217, 217, 217);
       border-radius: 6px;
       margin-bottom: 10px;
-      .visit-list {
-        display: flex;
-        align-items: center;
-        border-top: 1px solid rgb(217, 217, 217);
-        border-bottom: 1px solid rgb(217, 217, 217);
-        background: rgb(250, 250, 250);
-        padding-left: 10px;
-        .checkbox {
-          color: #494949;
-          font-size: 15px;
-          background: none;
-          cursor: default;
+      overflow: hidden;
+      .visit-list-wrap {
+        :not(:last-of-type) {
+          border-bottom: 1px solid rgb(217, 217, 217);
         }
-        .input {
-          border: none;
-          background: none;
-          color: #494949;
-          font-size: 16px;
-          font-weight: 800;
-          cursor: default;
-          height: 42px;
-        }
-      }
-      .checkList-wrap {
-        padding: 6px 0;
-        .check-list {
+        .visit-list {
           display: flex;
           align-items: center;
-          margin-left: 20px;
-          div {
-            display: flex;
-            align-items: center;
-            height: 27px;
-          }
+          background: rgb(250, 250, 250);
+          padding-left: 10px;
           .checkbox {
             color: #494949;
+            font-size: 15px;
             background: none;
             cursor: default;
           }
@@ -155,10 +139,38 @@ export const DetailScheduleDiv = styled.div`
             border: none;
             background: none;
             color: #494949;
-            font-weight: 400;
-            font-size: 14px;
-            line-height: 0.8;
+            font-size: 16px;
+            font-weight: 800;
             cursor: default;
+            height: 42px;
+          }
+        }
+        .checkList-wrap {
+          padding: 6px 0;
+          border-top: 1px solid rgb(217, 217, 217);
+          .check-list {
+            display: flex;
+            align-items: center;
+            margin-left: 20px;
+            div {
+              display: flex;
+              align-items: center;
+              height: 27px;
+            }
+            .checkbox {
+              color: #494949;
+              background: none;
+              cursor: default;
+            }
+            .input {
+              border: none;
+              background: none;
+              color: #494949;
+              font-weight: 400;
+              font-size: 14px;
+              line-height: 0.8;
+              cursor: default;
+            }
           }
         }
       }
